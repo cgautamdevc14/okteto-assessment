c090ad3fc7fc2da4794f4c344d064bfb8055fa7d
Gautamdev Chowdary <gchowdar@asu.edu>
2023-02-17 11:56:44 -0800

    deplotyment

--- main.go
+++ main.go
@@ -4,12 +4,27 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
 
+type KubePod struct {
+	name     string
+	restarts int32
+	age      time.Duration
+}
+
+func NewKubePod(name string, restarts int32, age time.Duration) *KubePod {
+	return &KubePod{
+		name:     name,
+		restarts: restarts,
+		age:      age,
+	}
+}
 func main() {
 	fmt.Println("Starting hello-world server...")
 	http.HandleFunc("/", helloServer)
@@ -40,4 +55,56 @@ func helloServer(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprint(w, "Reached here now...")
 	fmt.Fprint(w, "There are %d pods in the cluster\n", len(pods.Items))
+}
+
+func getPods(cs *kubernetes.Clientset) *v1.PodList {
+	pods, err := cs.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return pods
+}
+
+func getInClusterConfig() *rest.Config {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return config
+}
+
+func getKubeClientset(cfg *rest.Config) *kubernetes.Clientset {
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return clientset
+}
+
+func createKubePods(podList *v1.PodList) []KubePod {
+	kubePods := make([]KubePod, 0, len(podList.Items))
+
+	for _, pod := range podList.Items {
+		podCreationTime := pod.GetCreationTimestamp()
+		podStatus := pod.Status
+		var restarts int32
+
+		name := pod.GetName()
+		age := time.Since(podCreationTime.Time).Round(time.Second)
+
+		for container := range pod.Spec.Containers {
+			restarts += podStatus.ContainerStatuses[container].RestartCount
+		}
+
+		kube := NewKubePod(name, restarts, age)
+		kubePods = append(kubePods, *kube)
+
+	}
+
+	return kubePods
 }
