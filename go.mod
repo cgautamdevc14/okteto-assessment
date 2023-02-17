aea146f5bf8ae1111a286a7574f01ed05cbdfd99
C Gautamdev Chowdary <48221514+cgautamdevc14@users.noreply.github.com>
2023-02-16 21:31:37 -0800

    Testing client-go library

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/okteto/go-getting-started
 
 go 1.17
+
+require k8s.io/client-go v0.26.1 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"context"
 )
 
 func main() {
@@ -15,4 +20,23 @@ func main() {
 
 func helloServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello world!")
+
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+		fmt.Fprint("E1")
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+		fmt.Fprint("E2")
+	}
+
+	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+		fmt.Fprint("E3")
+	}
+	fmt.Fprint("There are %d pods in the cluster\n", len(pods.Items))
 }
